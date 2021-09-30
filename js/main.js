082dba41ba2b7264758404701194e0a6014390c9
Oscar España <espaaoscar@gmail.com>
2021-09-30 10:43:49 -0500

    feat: create a transition in navigation links

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
-function init() {
+gsap.registerPlugin(ScrollTrigger);
 
+function initNavigation() {
+  const mainNavLink = gsap.utils.toArray(".main-nav a");
+  mainNavLink.forEach((link) => {
+    link.addEventListener("mouseleave", function (event) {
+      //add class
+      link.classList.add("animate-out");
+      setTimeout(() => {
+        //remove class after 300ms
+        link.classList.remove("animate-out");
+      }, 300);
+    });
+  });
+}
+
+function init() {
+  initNavigation();
 }
 
 window.addEventListener("load", function () {
