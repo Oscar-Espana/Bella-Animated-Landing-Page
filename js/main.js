039ed8ea7751665ebeb9aaabb2363fb6402254df
Oscar España <espaaoscar@gmail.com>
2021-10-01 08:38:17 -0500

    feat: Navigation Away

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ gsap.registerPlugin(ScrollTrigger);
 
 function initNavigation() {
   const mainNavLink = gsap.utils.toArray(".main-nav a");
+  const mainNavLinkRev = gsap.utils.toArray(".main-nav a").reverse();
   mainNavLink.forEach((link) => {
     link.addEventListener("mouseleave", function (event) {
       //add class
@@ -13,12 +14,28 @@ function initNavigation() {
     });
   });
 
+  function navAnimation(direction) {
+    console.log("direction", direction);
+    const scrollingDonw = direction === 1;
+    const links = scrollingDonw ? mainNavLink : mainNavLinkRev;
+    return gsap.to(links, {
+      duration: 0.5,
+      stagger: 0.075, //tambalear
+      autoAlpha: () => (scrollingDonw ? 0 : 1), // 0: hidden, 1: visible
+      y: () => (scrollingDonw ? 20 : 0),
+      ease: "power4.out"
+    });
+  }
+
   ScrollTrigger.create({
     start: 100,
+    end: "bottom bottom-=100",  //it means: marker is en end of body tag and scroll-end is in the bottom of page -100px 
     toggleClass: {
-        targets: "body",
+      targets: "body",
       className: "has-scrolled",
     },
+    onEnter: ({ direction }) => navAnimation(direction),
+    onLeaveBack: ({ direction }) => navAnimation(direction),
     markers: true,
   });
 }
